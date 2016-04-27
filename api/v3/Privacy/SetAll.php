4a7359e5e10046db1ecd60f2b426c37843bf1ae8
kewljuice <kewljuice@hotmail.com>
2016-04-27 16:41:07 +0200

    Created SetAll API call

--- api/v3/Privacy/SetAll.php
+++ api/v3/Privacy/SetAll.php
@@ -5,17 +5,31 @@
  *
  * http://booki.flossmanuals.net/civicrm-developer-guide/api/
  */
-function civicrm_api3_privacy_set_all($params) {
+function civicrm_api3_privacy_set_all($params)
+{
   // Set mandatory fields.
   civicrm_api3_verify_mandatory($params, NULL, array('contact_id',));
 
   // Execute function.
   try {
     // Create return array.
-    $values = array();
-    $params["method"] = "all";
+    $contact = $params['contact_id'];
+
+    // Execute SetAddress
+    $addressAPI = civicrm_api3('Privacy', 'set_address', array('sequential' => 1, 'contact_id' => $contact));
+
+    // Execute SetEmail
+    $emailAPI = civicrm_api3('Privacy', 'set_email', array('sequential' => 1, 'contact_id' => $contact));
+
+    // Get default value for phone from custom fields.
+    $object = array("Address" => $addressAPI['values'], "Email" => $emailAPI['values']);
+
+    // Create return array.
+    $values = array($object);
+
     // Execute.
-    return civicrm_api3_create_success($values, $params);
+    return civicrm_api3_create_success($values, $params, 'set_all', 'create');
+
   } catch (Exception $e) {
     // Exception.
     return civicrm_api3_create_error('Caught exception: ', $e->getMessage(), '\n');
@@ -28,7 +42,8 @@ function civicrm_api3_privacy_set_all($params) {
  * @param array $params
  *   Array of parameters determined by getfields.
  */
-function _civicrm_api3_privacy_set_all_spec(&$params) {
+function _civicrm_api3_privacy_set_all_spec(&$params)
+{
   // We declare all these pseudoFields as there are other undocumented fields accessible via the api.
   $params['contact_id'] = array(
     'title' => 'Contact ID',
